0f4dfd5b572a22450b61c2370e172499ac20bba4
marisahonkai <marisahonkai@gmail.com>
2025-03-25 13:30:20 +0800

    82 supply

---
+++ src/routes/calendar82/+layout.server.js
@@ -0,0 +1,14 @@
+
+export async function load({ url }) {
+    // Check for the query parameter
+    const isBoss = url.searchParams.has('boss');
+
+    // Return dynamic OG image based on the query parameter
+    const ogImage = isBoss
+        ? 'https://i.imgur.com/woyZNL4.png'
+        : 'https://i.imgur.com/UmmqNyZ.png';
+
+    return {
+        ogImage
+    };
+}
