d2289ec1f85a04706384d023d742801268e1a335
marisahonkai <marisahonkai@gmail.com>
2025-04-04 16:23:08 +0800

    Update bossData.js

--- src/lib/bossData.js
+++ src/lib/bossData.js
@@ -1237,6 +1237,18 @@ export const teamData = {
   },
   bleedhos: {
     teamDataSets: [
+      {
+        tabname: "v8.2 CN",
+        teamInfoText: "Data is from v8.2 CN Server (PC) Top 50 Only, Myriad ??? D. ",
+      entries: [
+        {
+          type: 'lineup',
+          lineup: ["images/valkportrait/ps.png", "images/valkportrait/kiana badum.png", "images/valkportrait/Vita Lone Planetfarer.png", "images/valkportrait/asop_teri.png"],
+          rank: '1+',
+          pct: '100%',
+          bottext: "Highest Score: S 783, S2 786, SS 809, 9S 835 "
+        }
+      ]},
     {
       tabname: "v7.8 SEA",
       teamInfoText: "Data is from v7.8 SEA Server Top 100 Myriad 533 D.",
