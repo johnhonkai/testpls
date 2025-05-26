6d06ff8913b04cfc65c0c79ba8c93c456ce991cb
marisahonkai <marisahonkai@gmail.com>
2025-05-26 15:15:54 +0800

    Update +layout.server.js

--- src/routes/calendar82/+layout.server.js
+++ src/routes/calendar82/+layout.server.js
@@ -5,7 +5,7 @@ export async function load({ url }) {
 
     // Return dynamic OG image based on the query parameter
     const ogImage = isBoss
-        ? 'https://i.imgur.com/wtVFoa3.png'
+        ? 'https://i.imgur.com/WNkDz4Z.png'
         : 'https://i.imgur.com/bIscDGD.png';
 
     return {
