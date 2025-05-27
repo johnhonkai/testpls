9bbc4fbe6242e6b324639b5ff8a52f1d48085abe
marisahonkai <marisahonkai@gmail.com>
2025-05-27 12:55:09 +0800

    calendar update

--- src/lib/components/VersionSelect.svelte
+++ src/lib/components/VersionSelect.svelte
@@ -2,8 +2,8 @@
     import { goto } from '$app/navigation';
     import { onMount } from 'svelte';
 
-    let versions = ["8.2", "8.1", "8.0" , "7.9", "7.8"];
-    export let currentVersion = "8.0"; // Set current version
+    let versions = ["8.3" , "8.2", "8.1", "8.0" , "7.9", "7.8"];
+    export let currentVersion = "8.3"; // Set current version
 
     function changeVersion(event) {
         const newVersion = event.target.value;
