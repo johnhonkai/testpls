5243064e27ef7ab556b2999794c7e9e9d89ad99c
marisahonkai <marisahonkai@gmail.com>
2025-05-30 15:54:06 +0800

    Update +page.svelte

--- src/routes/boss/+page.svelte
+++ src/routes/boss/+page.svelte
@@ -22,8 +22,8 @@
   import BossDetails from '$lib/components/BossDetails.svelte';
   import TeamDisplay from '$lib/components/TeamDisplay.svelte';
 
-  let selectedWeather = "stellar"; // Default weather
-  let selectedBoss = "stellarassaka"; // Default boss
+  let selectedWeather = "quantum"; // Default weather
+  let selectedBoss = "quavalralhal"; // Default boss
   let isLoading = true;
   let isFirstLoad = true; // Flag to distinguish initial load
 
@@ -34,7 +34,7 @@
     const bossParam = queryParams[0];
     if (bossParam && bossData[bossParam]) {
       selectedBoss = bossParam;
-      selectedWeather = findWeatherByBossId(bossParam) || "stellar";
+      selectedWeather = findWeatherByBossId(bossParam) || "quantum";
     }
     // Wait for initial images to load
     checkImagesLoaded();
