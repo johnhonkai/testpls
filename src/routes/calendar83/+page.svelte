bc78224b1a12311fca166ef2e2131eebb5d74e48
marisahonkai <marisahonkai@gmail.com>
2025-06-03 16:55:32 +0800

    update calendar

--- src/routes/calendar83/+page.svelte
+++ src/routes/calendar83/+page.svelte
@@ -377,9 +377,9 @@
                 </div>
                      
                     <div class="flex col-span-3 justify-center gap-1 ml-2">
-                        <BossCard />
-                        <BossCard />
-                        <BossCard   />
+                        <BossCard image="/images/Bosses/Sampo.webp" date="Jun 27"  />
+                        <BossCard image="/images/Bosses/Meteor_Guaymas_SSS.webp" />
+                        <BossCard image="/images/Bosses/Herrscher of the Void.webp"   />
                     </div>
     
 
