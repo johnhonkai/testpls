7398d13e77ca6bdd4dff624864bcc202ca0677f6
marisahonkai <marisahonkai@gmail.com>
2025-07-04 23:13:25 +0800

    asd

--- src/routes/valk/scoralie/+page.svelte
+++ src/routes/valk/scoralie/+page.svelte
@@ -439,7 +439,7 @@ async function increaseLike() {
                 <div class="relative overflow-hidden" style="padding-top: 56.25%;">
             <iframe
                 class="absolute top-0 left-0 w-full h-full"
-                src="https://www.youtube.com/embed/"
+                src="https://www.youtube.com/embed/YKBSn11HKpI"
                 title="YouTube video player"
                 frameborder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
