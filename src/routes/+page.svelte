91be171f877f9727fe70e1cf945f1574393bd6df
marisahonkai <marisahonkai@gmail.com>
2025-08-08 13:14:52 +0800

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -362,13 +362,13 @@
         </div>
       </a>
   
-      <a href="https://www.youtube.com/watch?v=WJAHvgl4noA" id="news3" class="group block rounded-2xl bg-zinc-800 border border-zinc-600/50 hover:border-zinc-400 transition-colors duration-200 overflow-hidden">
+      <a href="https://www.youtube.com/watch?v=8NufpnRYd08" id="news3" class="group block rounded-2xl bg-zinc-800 border border-zinc-600/50 hover:border-zinc-400 transition-colors duration-200 overflow-hidden">
         <div class="overflow-hidden h-48">
-          <img src="https://i.ytimg.com/vi/WJAHvgl4noA/hqdefault.jpg" alt="Astral Ring" class="w-full h-full object-cover transition-transform duration-300 group-hover:scale-110" />
+          <img src="https://i.ytimg.com/vi/8NufpnRYd08/hqdefault.jpg" alt="Astral Ring" class="w-full h-full object-cover transition-transform duration-300 group-hover:scale-110" />
         </div>
         <div class="p-4">
-          <h3 class="text-lg font-semibold text-white mb-1">8.4 News</h3>
-          <p class="text-slate-300 text-md">AstralOp Chenxue</p>
+          <h3 class="text-lg font-semibold text-white mb-1">New Elysia</h3>
+          <p class="text-slate-300 text-md">OMG she's back!</p>
         </div>
       </a>
   
@@ -387,9 +387,9 @@
   
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
   
-      <a href="https://www.youtube.com/watch?v=gYRI5gO67H8" id="content1" target="_blank" class="group block rounded-2xl bg-zinc-800 border border-zinc-600/50 hover:border-zinc-400 transition-colors duration-200 overflow-hidden">
+      <a href="https://www.youtube.com/watch?v=8NufpnRYd08" id="content1" target="_blank" class="group block rounded-2xl bg-zinc-800 border border-zinc-600/50 hover:border-zinc-400 transition-colors duration-200 overflow-hidden">
         <div class="overflow-hidden h-48">
-          <img src="https://i.ytimg.com/vi/gYRI5gO67H8/hqdefault.jpg" alt="Latest Video" class="w-full h-full object-cover transition-transform duration-300 group-hover:scale-110" />
+          <img src="https://i.ytimg.com/vi/8NufpnRYd08/hqdefault.jpg" alt="Latest Video" class="w-full h-full object-cover transition-transform duration-300 group-hover:scale-110" />
         </div>
         <div class="p-4">
           <h3 class="text-lg font-semibold text-white mb-1">Latest Video From Earth</h3>
