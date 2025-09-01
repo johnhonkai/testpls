a516e568d11a029e329f563f4d150f2ea2232ef3
marisahonkai <marisahonkai@gmail.com>
2025-09-01 17:26:46 +0800

    Update bossDate.js

--- src/lib/bossDate.js
+++ src/lib/bossDate.js
@@ -21,54 +21,55 @@ export const bossSchedule = {
     },    
   ] ,  
 
-  "v8.4": [
-    {
-      boss: "ignite-benares",
-      weather: "ignite",
-      start: "2025-08-22T15:00:00+08:00",
-      end: "2025-08-25T15:00:00+08:00"
-    },    
-    {
-      boss: "lantern",
-      weather: "flux",
-      start: "2025-08-25T15:00:00+08:00",
-      end: "2025-08-29T15:00:00+08:00"
-    },
-    {
-      boss: "litost",
-      weather: "fire",
-      start: "2025-08-29T15:00:00+08:00",
-      end: "2025-09-1T15:00:00+08:00"
-    },
-    {
-      boss: "imgflower",
-      weather: "img",
-      start: "2025-09-1T15:00:00+08:00",
-      end: "2025-09-5T15:00:00+08:00"
-    },    
-    {
-      boss: "sushang",
-      weather: "paralyze",
-      start: "2025-09-5T15:00:00+08:00",
-      end: "2025-09-8T15:00:00+08:00"
-    },  
-    {
-      boss: "lightningsprout",
-      weather: "lightning",
-      start: "2025-09-8T15:00:00+08:00",
-      end: "2025-09-12T15:00:00+08:00"
-    },    
-    {
-      boss: "firetank",
-      weather: "fire",
-      start: "2025-09-12T15:00:00+08:00",
-      end: "2025-09-15T15:00:00+08:00"
-    },      
-    {
-      boss: "hoice",
-      weather: "ignite",
-      start: "2025-09-15T15:00:00+08:00",
-      end: "2025-09-19T15:00:00+08:00"
-    },           
-  ]
+"v8.4": [
+  {
+    boss: "ignite-benares",
+    weather: "ignite",
+    start: "2025-08-22T15:00:00+08:00",
+    end: "2025-08-25T15:00:00+08:00"
+  },    
+  {
+    boss: "lantern",
+    weather: "flux",
+    start: "2025-08-25T15:00:00+08:00",
+    end: "2025-08-29T15:00:00+08:00"
+  },
+  {
+    boss: "litost",
+    weather: "fire",
+    start: "2025-08-29T15:00:00+08:00",
+    end: "2025-09-01T15:00:00+08:00"  
+  },
+  {
+    boss: "imgflower",
+    weather: "imaginary",
+    start: "2025-09-01T15:00:00+08:00", 
+    end: "2025-09-05T15:00:00+08:00"   
+  },    
+  {
+    boss: "sushang",
+    weather: "paralyze",
+    start: "2025-09-05T15:00:00+08:00", 
+    end: "2025-09-08T15:00:00+08:00"   
+  },  
+  {
+    boss: "lightningsprout",
+    weather: "lightning",
+    start: "2025-09-08T15:00:00+08:00", 
+    end: "2025-09-12T15:00:00+08:00"  
+  },    
+  {
+    boss: "firetank",
+    weather: "fire",
+    start: "2025-09-12T15:00:00+08:00", 
+    end: "2025-09-15T15:00:00+08:00"   
+  },      
+  {
+    boss: "hoice",
+    weather: "ignite",
+    start: "2025-09-15T15:00:00+08:00",
+    end: "2025-09-19T15:00:00+08:00"   
+  }           
+]
+
 };
