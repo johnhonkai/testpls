a9cccc2cabd253c43bbd47befe12de904c3d02cd
marisahonkai <marisahonkai@gmail.com>
2025-09-09 18:22:29 +0800

    Update +layout.server.js

--- src/routes/calendar84/+layout.server.js
+++ src/routes/calendar84/+layout.server.js
@@ -4,8 +4,8 @@ export async function load({ url }) {
     const isBoss = url.searchParams.has('boss');
 
     const ogImage = isBoss
-        ? 'https://i.imgur.com/vR1cT4C.png'
-        : 'https://i.imgur.com/Lb76zGr.jpeg';
+        ? 'https://i.imgur.com/vR1cT4C.png?ver=2'
+        : 'https://i.imgur.com/Lb76zGr.jpeg?ver=2';
 
     return {
         ogImage
