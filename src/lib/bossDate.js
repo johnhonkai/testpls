38113b34be8f4764fcdb8cc39674c4e348d63236
marisahonkai <marisahonkai@gmail.com>
2025-10-17 19:09:14 +0800

    Update bossDate.js

--- src/lib/bossDate.js
+++ src/lib/bossDate.js
@@ -18,11 +18,108 @@ export const bossSchedule = {
       weather: "stardust",
       start: "2025-08-18T15:00:00+08:00",
       end: "2025-08-22T15:00:00+08:00"
-    },    
+    }    
   ] ,  
 
 "v8.4": [
-     {
+  {
+    boss: "ignite-benares",
+    weather: "ignite",
+    start: "2025-08-22T15:00:00+08:00",
+    end: "2025-08-25T15:00:00+08:00"
+  },    
+  {
+    boss: "lantern",
+    weather: "flux",
+    start: "2025-08-25T15:00:00+08:00",
+    end: "2025-08-29T15:00:00+08:00"
+  },
+  {
+    boss: "litost",
+    weather: "fire",
+    start: "2025-08-29T15:00:00+08:00",
+    end: "2025-09-01T15:00:00+08:00"  
+  },
+  {
+    boss: "imgflower",
+    weather: "imaginary",
+    start: "2025-09-01T15:00:00+08:00", 
+    end: "2025-09-05T15:00:00+08:00"   
+  },    
+  {
+    boss: "sushang",
+    weather: "paralyze",
+    start: "2025-09-05T15:00:00+08:00", 
+    end: "2025-09-08T15:00:00+08:00"   
+  },  
+  {
+    boss: "lightningsprout",
+    weather: "lightning",
+    start: "2025-09-08T15:00:00+08:00", 
+    end: "2025-09-12T15:00:00+08:00"  
+  },    
+  {
+    boss: "firetank",
+    weather: "fire",
+    start: "2025-09-12T15:00:00+08:00", 
+    end: "2025-09-15T15:00:00+08:00"   
+  },      
+  {
+    boss: "hoice",
+    weather: "ignite",
+    start: "2025-09-15T15:00:00+08:00",
+    end: "2025-09-19T15:00:00+08:00"   
+  },      
+  {
+    boss: "sa",
+    weather: "stardust",
+    start: "2025-09-19T15:00:00+08:00",
+    end: "2025-09-22T15:00:00+08:00"   
+  },    
+   {
+    boss: "phyhod",
+    weather: "physical",
+    start: "2025-09-22T15:00:00+08:00",
+    end: "2025-09-26T15:00:00+08:00"   
+  },  
+   {
+    boss: "icehellmaru",
+    weather: "ice",
+    start: "2025-09-26T15:00:00+08:00",
+    end: "2025-09-29T15:00:00+08:00"   
+  }, 
+   {
+    boss: "quavalrahal",
+    weather: "quantum",
+    start: "2025-09-29T15:00:00+08:00",
+    end: "2025-10-03T15:00:00+08:00"   
+  },   
+    {
+    boss: "husk-hitcount",
+    weather: "lightning",
+    start: "2025-10-03T15:00:00+08:00",
+    end: "2025-10-06T15:00:00+08:00"   
+  },  
+    {
+    boss: "phyalien",
+    weather: "physical",
+    start: "2025-10-06T15:00:00+08:00",
+    end: "2025-10-10T15:00:00+08:00"   
+  },    
+    {
+    boss: "igniteandrius",
+    weather: "ignite",
+    start: "2025-10-10T15:00:00+08:00",
+    end: "2025-10-13T15:00:00+08:00"   
+  },  
+    {
+    boss: "starrimestar",
+    weather: "starless",
+    start: "2025-10-13T15:00:00+08:00",
+    end: "2025-10-17T15:00:00+08:00"   
+  },  
+  
+{
     boss: "resoguaymas",
     weather: "resonance",
     start: "2025-10-17T15:00:00+08:00",
@@ -69,7 +166,7 @@ export const bossSchedule = {
     weather: "lightning",
     start: "2025-11-10T15:00:00+08:00",
     end: "2025-11-13T15:00:00+08:00"   
-  },        
+  }          
 ]
 
 };
