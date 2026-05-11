0e106d50585b3701626d5b70d79fa07b337b0460
marisahonkai <marisahonkai@gmail.com>
2026-05-11 17:26:18 +0800

    Update +page.svelte

--- src/routes/valk/89seele/+page.svelte
+++ src/routes/valk/89seele/+page.svelte
@@ -295,7 +295,8 @@ async function increaseLike() {
 
 >
   <CharBio mode="dps" rank="s" type="sd" element="lightning" ar="ws" bg="bg-blue-800" />
-  <CharBio mode="support" ar={['ws']} bg="bg-blue-800" />
+  <CharBio mode="support" ar={['loa', 'wod' , 'ws']} bg="bg-blue-800" />
+  
 </CharacterLayout>
 
 
