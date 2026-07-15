5ba6d05e055f5699fffc92317665f00a73276a8b
marisahonkai <marisahonkai@gmail.com>
2026-07-15 10:02:15 +0800

    Update +page.svelte

--- src/routes/calendar89/+page.svelte
+++ src/routes/calendar89/+page.svelte
@@ -500,7 +500,7 @@
                 <div class="flex font-bold  text-zinc-800 bg-white border-b-[2px] border-slate-300 items-center justify-center text-center leading-4">                   
                     <span class="absolute w-[30px] h-[4px] bg-pink-600 inline-block top-[-3px] left-0"></span>
                     
-                   <span class="font-bold0">EVENT 3</span>
+                   <span class="font-bold0">EVENT 5</span>
                 </div> 
                 <div></div>
                 <div></div>
@@ -520,7 +520,7 @@
             <div class="relative grid grid-cols-9 gap-1 h-[54px] pe-1 overflow-x-clip">
                 <div class="flex font-bold  text-pink-600 bg-white border-b-[2px] border-slate-300 items-center justify-center text-center leading-4">
                     <span class="absolute w-[30px] h-[4px] bg-pink-600 inline-block top-[-3px] left-0"></span>
-                     <span class="font-bold0">EVENT 5</span>
+                     <span class="font-bold0">EVENT 6</span>
                 </div> 
                     <div class="col-span-3"></div>
                     <div on:click={() => openModal("")} class="col-span-5 rounded-lg will-change-transform transform transition-transform hover:scale-105 cursor-pointer relative my-0.5  bg-gray-100 text-black font-semibold text-center items-center justify-center border-2 border-pink-700">
@@ -537,7 +537,7 @@
             <div class="relative grid grid-cols-9 gap-1 h-[54px] pe-1 overflow-x-clip">
                 <div class="flex font-bold  text-zinc-800 bg-white border-b-[2px] border-slate-300 items-center justify-center text-center leading-4">
                     <span class="absolute w-[30px] h-[4px] bg-pink-600 inline-block top-[-3px] left-0"></span>
-                     <span class="font-bold0">EVENT 6</span>
+                     <span class="font-bold0">EVENT 7</span>
                 </div> 
                     <div class="col-span-4"></div>
                     <div on:click={() => openModal("")} class="col-span-4 rounded-lg will-change-transform transform transition-transform hover:scale-105 cursor-pointer relative my-0.5  bg-gray-100 text-black font-semibold text-center items-center justify-center border-2 border-pink-700">
